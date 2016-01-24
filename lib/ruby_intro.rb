027c8d40c779874cb3dabcb6cb9335917e5bfa90
Warren Tian <warrent888@berkeley.edu>
2016-01-24 20:58:20 +0000

    completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,110 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  total = 0
+  arr.each do |x|
+    total += x
+  end
+  return total
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  best = arr.max
+  arr.delete(best)
+  return best + arr.max
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length == 0
+    if n == 0
+      return true
+    end
+    return false
+  end
+  if arr.length == 1
+    return false
+  end
+  arr.each_with_index do |val, i|
+    arr.each_with_index do |val, j|
+      if i == j
+        next
+      end
+      if (arr[i] + arr[j]) == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s =~ /\A[b-df-hj-np-tv-z]/ or s =~ /\A[B-DF-HJ-NP-TV-Z]/
+    return true
+  end
+  return false
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if s !~ /^[0-1]+/
+    return false
+  end
+  if s.length < 2
+    if s == "0"
+      return true
+    end
+    return false
+  end
+  last_two = s.reverse[0...2].reverse
+  if last_two == "00"
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.length < 1 or price <= 0
+      raise ArgumentError.new("Invalid argument")
+    end
+    @isbn = isbn
+    @price = price
+  end
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+  
+  def price=(price)
+    @price = price
+  end
+  
+  def price_as_string
+    price = @price.to_s
+    if price.include? "."
+      last_two = price.reverse[0...2].reverse
+      if last_two.include? "."
+        price += "0"
+      end
+    else
+      price += ".00"
+    end
+    return ("$" + price)
+  end
 end
+
